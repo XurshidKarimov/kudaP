84056098a4d1b60437833e549cfbf8da7082b585
Xurshid <kaxudi34801@gmail.com>
2023-01-08 16:36:12 +0500

    slider fixing

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
       perPage: 3,
       gap: '1rem',
       autoScroll: {
-        speed: 1.5,
+        speed: 0,
       },
     }).mount(window.splide.Extensions);
   });
