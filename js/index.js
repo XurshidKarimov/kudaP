d89049158ec2ed6a2b799292c3de35387d2783e9
Xurshid <kaxudi34801@gmail.com>
2023-01-08 20:53:10 +0500

    slider content done. 2 sliders done.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-    new Splide('.splide', {
+    new Splide('#splide1', {
+      type   : 'loop',
+      drag   : 'free',
+      focus  : 'center',
+      perPage: 3,
+      gap: '1rem',
+      height: '17rem',
+      autoScroll: {
+        speed: 1.5,
+      },
+    }).mount(window.splide.Extensions);
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#splide2', {
       type   : 'loop',
       drag   : 'free',
       focus  : 'center',
